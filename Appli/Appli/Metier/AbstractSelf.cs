59bc213f225ebae47b951bde5ca3edde81b33a37
Bastien <bastien.gandboeuf@laposte.net>
2017-02-06 09:50:01 +0100

    Ajout abstraction

---
+++ Appli/Appli/Metier/AbstractSelf.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appli.Metier
+{
+    public abstract class AbstractSelf
+    {
+        private DateTime dateDuJour = DateTime.Today;
+        private List<AbsMenu> menus;
+        private List<AbsPlat> plats;
+        private List<AbsProduit> produits;
+        private String droitUtilisateur;
+        private AbsUsager client;
+        private AbsMenu menuDuJour;
+
+
+    }
+}
