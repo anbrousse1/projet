8546988509891382b9911d3311c5e8ea8c26bf3e
Jérémy <jeremy.tessanne@etu.udamail.fr>
2017-02-23 13:33:17 +0100

    Vues AjouterPlat, AjouterUtilisateur et ModifierTicket

---
+++ ProjetSelf/ProjetSelf/ModifierTicket.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vue
+{
+    /// <summary>
+    /// Logique d'interaction pour ModifierTicket.xaml
+    /// </summary>
+    public partial class ModifierTicket : UserControl
+    {
+        private MainWindow parent;
+        public ModifierTicket(MainWindow m)
+        {
+            parent = m;
+            InitializeComponent();
+            mgrid.Children.Add(new Recapitulatif(parent));
+
+        }
+
+        public void clickModifier(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        public void clickAnnuler(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
